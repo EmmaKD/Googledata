a5be9f4d029a37ada0f91229062a3c56780309fd
Emmanuel <edogbevi@gmail.com>
2014-02-27 10:58:39 +0000

    edited javascript file

---
+++ Mygoogledat.js
@@ -0,0 +1,15 @@
+/**
+ * @author
+ */
+
+// The following are the project steps
+
+//I will set up document ready
+//And load Google chart package
+//I will then load the data
+//And then render the chart
+
+function(pageLoaded);
+
+console.log("Creating Google data");
+
